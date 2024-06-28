035691e898078772b45a964f8161664b6c43ae5b
fevse <fedulov.v.s@gmail.com>
2024-06-29 00:01:45 +0400

    draft version

--- cmd/todolist/main.go
+++ cmd/todolist/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/fevse/todo_list/internal/app"
+	"github.com/fevse/todo_list/internal/storage"
+)
+
+var (
+	command string
+)
+
+func init() {
+	flag.StringVar(&command, "command", "", "write command to execute")
+}
 
 func main() {
+	flag.Parse()
 	fmt.Println("***TODO LIST***")
+
+	storage := storage.New()
+	app := app.New(storage)
+
+	switch command {
+	case "list":
+		tasks, err := app.Storage.ShowList()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for _, t := range tasks {
+			fmt.Printf("#%d %s - %s: %v\n", t.ID, t.Title, t.Status, t.Created.Format("02.01.2006 15:04:05"))
+		}
+	case "create":
+		var t, s string
+		fmt.Println("Add new task")
+		fmt.Print("Title: ")
+		fmt.Scan(&t)
+		fmt.Print("Status: ")
+		fmt.Scan(&s)
+		err := app.Storage.CreateTask(t, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	default:
+		fmt.Println("need command to execute")
+	}
+
+	fmt.Println("THE END")
 }
