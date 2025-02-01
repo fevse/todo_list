b75da7874a353f03f43db681c8456eef32c78dad
fevse <fedulov.v.s@gmail.com>
2025-02-01 23:44:37 +0400

    some fix

--- cmd/todolist/main.go
+++ cmd/todolist/main.go
@@ -6,15 +6,20 @@ import (
 
 	"github.com/fevse/todo_list/internal/app"
 	"github.com/fevse/todo_list/internal/cli"
+	"github.com/fevse/todo_list/internal/config"
 	"github.com/fevse/todo_list/internal/storage"
 )
 
 func main() {
 	flag.Parse()
 	fmt.Println("***TODO LIST***")
-
-	storage := storage.New()
+	conf, err := config.NewConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	storage := storage.New(conf)
 	app := app.New(storage)
-	app.Storage.Migrate("db/migrations")
+	app.Storage.Migrate()
 	cli.Cli(*app)
 }
